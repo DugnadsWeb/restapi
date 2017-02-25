5d4f54cfbf8c596bc2906c2d52c6af71b5f945b7
Alexander Kjeserud <ole.alexander89@gmail.com>
2017-02-25 15:05:11 +0100

    substantioal restructure and improvements to neo4j interface

--- api/models/neo4j_base.js
+++ api/models/neo4j_base.js
@@ -20,36 +20,32 @@ var db_base = function(){
   // Creates the object in the database for the first time
   this.create = function(callback){
     if (this.validate(this.db_fields)){
-      var query = build_create_query(this);
+      var query = "CREATE " + this.make_query_object('a', {use_all: true});
       var session = driver.session();
       session.run(query)
         .then((result) => {
-          console.log(result);
           session.close();
           callback(200, 'Object Creaated');
         })
         .catch((err) => {
           session.close();
           callback(401, err);
-          console.log(err);
         });
     }else {
       callback(400, "Object does not validate");
     }
   }
 
-
   // gets the object
   // requires that the object has at least one unique field!
+  // @deprecated use db_base.get_unique
   this.read = function(callback){
-    var query = build_get_query(this);
-    console.log(query);
+    var query = "MATCH " + this.make_query_object('a');
     var session = driver.session();
     session.run(query)
       .then((result) => {
         session.close();
         if (!!result.records.length != 0){
-          console.log(result.records);
           callback(200, result.records[0]._fields[0].properties);
         }else{
           callback(404, "email or password missmatch");
@@ -62,46 +58,73 @@ var db_base = function(){
   }
 
   // updates the object
-  this.update = function(callback){
-      var query = build_update_query(this);
+  // TODO need to test and approve edit data. proposal: make validate_update method
+  this.update = function(edited_object){
+    return new Promise((res, rej) => {
+      var query = "MATCH " + this.make_query_object('a') +
+        build_update_query(edited_object);
       var session = driver.session();
       session.run(query)
         .then(() => {
           session.close();
-          callback(200, 'Object updated');
+          res(this.constructor.name + ' updated');
         })
         .catch((err) => {
           session.close();
-          callback(400, err);
+          rej(err);
         });
+    });
   }
 
   // delete the object
-  this.delete = function(callback){
-    var query = build_match_clause(this) + " delete a";
-    console.log(query);
-    var session = driver.session();
-    session.run(query)
-      .then(() => {
-        session.close();
-        callback(200, this.constructor.name + " was deleted");
-      })
-      .catch((err) => {
-        session.close();
-        callback(400, err);
-      });
+  this.delete = function(){
+    return new Promise((res, rej) => {
+      var query = "MATCH " + this.make_query_object('a') + " delete a";
+      var session = driver.session();
+      session.run(query)
+        .then(() => {
+          session.close();
+          res(this.constructor.name + " was deleted");
+        })
+        .catch((err) => {
+          session.close();
+          rej(err);
+        });
+    });
   }
+
+
   // TODO try to abstract this to sql if possible
   // arg: relationship ie join table in sql
-  this.get_realations_of_type = function(type, relationship, callback){
-    query = build_match_clause;
-    query += "-[b:" + relationship + "]->(c:" + type + ") return c";
+  this.get_realations_of_type = function(type, relationship){
+      return new Promise((res, rej) => {
+        query = "MATCH " + db_base.make_query_object(this, 'a') +
+        "-[b:" + relationship + "]-(c:" + type + ") return b, c";
+        let session = driver.session();
+        session.run(query)
+        .then((result) => {
+          session.close();
+          let ret = [];
+          for (let i=0; i < result.records.length;i++){
+            let field = {};
+            field[relationship] = result.records[i]._fields[0].properties;
+            field[type] = result.records[i]._fields[1].properties;
+            ret.push(field);
+          }
+          res(ret);
+        })
+        .catch((err) => {
+          session.close();
+          rej(err);
+        })
+      });
   }
 
 
-  this.create_relation = function(relate_object, relationship){
+  this.create_relation = function(relate_object, relationship, options){
     return new Promise((res, rej) => {
-      query = build_relation_query(this, relate_object, relationship);
+      query = create_relation_query(this, relate_object, relationship, options);
+
       session = driver.session();
       session.run(query)
         .then(() => {
@@ -116,45 +139,40 @@ var db_base = function(){
   }
 
 
-  // builds the create query for the object
-  function build_create_query(me){
-    var query = "CREATE (a:" + me.constructor.name + " { "
-    for (var field in me.db_fields){
-        query += field + ": '" + me.db_fields[field].data + "',";
-    }
-    return query.substring(0, query.length-1) + "})";
-  }
-
-  // builds the get query
-  // requires that the object has at least one unique field!
-  function build_get_query(me){
-    var query = build_match_clause(me);
-    query += "RETURN a";
-    return query;
-  }
-
   // builds the set query for the object
   function build_update_query(me){
-    var query = build_match_clause(me);
     query = " SET ";
-    for (var field in me.db_fields){
-      if (typeof me.db_fields[field] !== 'undefined'){
+    for (field in me.db_fields){
+      if (!!me.db_fields[field].data){
         query += "a." + field + " = \"" + me.db_fields[field].data + "\",";
       }
     }
     return query.substring(0, query.length-1);
   }
 
-  // builds the match clause
-  function build_match_clause(me){
-    // find object to edit
-    var query = "MATCH (a:" + me.constructor.name + " {";
-    for (var field in me.db_fields){
-      if (typeof me.db_fields[field] !== 'undefined'){
-        for (var i in me.db_fields[field].meta){
-          if (me.db_fields[field].meta[i] == 'unique' ||
-            me.db_fields[field].meta[i] == 'use'){
-            query += field + ": \"" + me.db_fields[field].data + "\",";
+
+  // options: unique - Creates a relationship if one does not all ready exists
+  function create_relation_query(me, relate_object, relationship, options){
+    options = !!options ? options : {};
+    query = "MATCH" + me.make_query_object('a') + ", " +
+      relate_object.make_query_object('b');
+    query += " CREATE " + (options.unique ? "UNIQUE" : "") +
+      " (a)-[r:" + relationship.constructor.name + "]->(b) return r";
+    console.log(query);
+    return query;
+  }
+
+  // options: use_all - uses all defined object fields
+  this.make_query_object = function (tag, options){
+    options = !!options ? options : {}
+    var query = "(" + (!!tag ? tag : '') + ":" + this.constructor.name + " {";
+    for (var field in this.db_fields){
+      if (typeof this.db_fields[field] !== 'undefined'){
+        for (var i in this.db_fields[field].meta){
+          if ((this.db_fields[field].meta[i] == 'unique' ||
+            this.db_fields[field].meta[i] == 'use' ||
+            options.use_all) && !!this.db_fields[field].data){
+            query += field + ": \"" + this.db_fields[field].data + "\",";
           }
         }
       }
@@ -162,26 +180,6 @@ var db_base = function(){
     return query.substring(0, query.length-1) + "})";
   }
 
-
-  function build_relation_query(me, relate_object, relationship){
-    // find object to edit
-    var query = build_match_clause(me);
-    query += ", (b:" + relate_object.constructor.name + " {";
-    for (var field in relate_object.db_fields){
-      if (typeof relate_object.db_fields[field] !== 'undefined'){
-        for (var i in relate_object.db_fields[field].meta){
-          if (relate_object.db_fields[field].meta[i] == 'unique' ||
-            relate_object.db_fields[field].meta[i] == 'use'){
-            query += field + ": \"" + relate_object.db_fields[field].data + "\",";
-          }
-        }
-      }
-    }
-    query = query.substring(0, query.length-1) + "})";
-    query += " CREATE (a)-[r:" + relationship.constructor.name + "]->(b) \
-      return r";
-    return query;
-  }
 
 
 // end of db_base
@@ -244,5 +242,19 @@ db_base._get_from_unique_identifier_query = function(id){
 db_base.blueprint = {};
 
 
+db_base.make_query_object = function (object, tag){
+  var query = "(" + (!!tag ? tag : '') + ":" + object.constructor.name + " {";
+  for (var field in object.db_fields){
+    if (typeof object.db_fields[field] !== 'undefined'){
+      for (var i in object.db_fields[field].meta){
+        if (object.db_fields[field].meta[i] == 'unique' ||
+          object.db_fields[field].meta[i] == 'use'){
+          query += field + ": \"" + object.db_fields[field].data + "\",";
+        }
+      }
+    }
+  }
+return query.substring(0, query.length-1) + "})";
+}
 
 module.exports = db_base;
